38c6818859ef8cc349326283e2e8782b5033528b
Clark <631441@bah.com>
2023-10-23 13:52:03 -0500

    added tensors for time, space, heat

--- core_1d.py
+++ core_1d.py
@@ -11,3 +11,27 @@ def lam(n):
 
 def u(x,t):
     return 100 * torch.e**(-lam(1)**2*t) * torch.sin((torch.pi/L)*x)
+
+
+
+train_til_time = 5
+num_heat_points = 161
+
+x = torch.linspace(0,80,num_heat_points) #change back to 161 later
+
+
+output = torch.tensor([])
+t_train = torch.tensor([])
+
+for t in range(train_til_time):
+    t_train = torch.cat((t_train, torch.tensor([t])),dim=0)
+    out = torch.tensor(u(x,t))
+    out = out.view(1,num_heat_points)
+    output = torch.cat((output,out),dim=0)
+
+print(t_train)
+
+print(output)
+
+
+
