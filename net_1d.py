f9d67d2aa2688f537b4e15527e9374643d208dc7
Clark <631441@bah.com>
2023-10-30 11:54:10 -0500

    update 1 dim net file

--- net_1d.py
+++ net_1d.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import core_1d
 import matplotlib.pyplot as plt
 
+alpha = core_1d.K / (core_1d.rho*core_1d.sigma)
 
 class Net_1d(nn.Module):
     'build mlp for ld heat solution'
@@ -24,26 +25,40 @@ class Net_1d(nn.Module):
 
         return out
     
-train_in = torch.linspace(0,300,300)
-train_in = train_in.view(300,1)
+#physics training set
+p_train_in = core_1d.train_input
 
 
-net = Net_1d(1,161,210)
+
+
+net = Net_1d(2,1,128)
 
 loss_func = nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters(),lr=.01)
 
-for val in range(5000):
-    outputs = net(train_in)
+for val in range(len(core_1d.train_output)):
 
-    loss = loss_func(outputs,core_1d.train_output)
+    # yhat_phys = net(p_train_in)
+    # du = torch.autograd.grad(yhat_phys,core_1d.train_input,torch.ones_like(yhat_phys),create_graph=True)[0]
+    # print(du)
+    # du2 = torch.autograd.grad(du,p_train_in,torch.ones_like(du),create_graph=True)[0]
+    # print(du2)
+    # phys_res = du - alpha * du2**2
+    # phys_loss = torch.mean(phys_res**2)
 
+
+    outputs = net(core_1d.train_input[val])
+    # print(core_1d.train_input[val])
+    # print(core_1d.train_output[val])
+
+    loss = loss_func(outputs,core_1d.train_output[val])# + phys_loss
+    print(loss)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
 
-net(torch.tensor([200.])).shape
 
-plt.plot(torch.linspace(0,80,161).detach().numpy(),net(torch.tensor([800.])).detach().numpy())
-plt.show()
+
+# plt.plot(torch.linspace(0,80,161).detach().numpy(),net(torch.tensor([800.])).detach().numpy())
+# plt.show()
