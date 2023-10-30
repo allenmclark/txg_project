beb32f949a329d32ef12531f3ddfdc7c383fa450
Clark <631441@bah.com>
2023-10-30 16:22:51 -0500

    updated for working 1d in sample version

--- visual.py
+++ visual.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import torch
 from net_1d import net
+import core_1d
 
 
 fig, ax = plt.subplots()
@@ -13,28 +14,26 @@ ax.set_ylim(0,100)
 line1, = ax.plot([],[],color='orange',lw=2)
 line2, = ax.plot([],[],color='blue',lw=2)
 
-# def init():
-#     line1.set_data([],[])
-#     line2.set_data([],[])
-#     return line1,line2
-
-
-
-def update(frame):
-    y2 = torch.tensor([])
-    for val in range(161):
-        torch.cat((y2,net(torch.tensor([frame*1.]))))
-
-    x = torch.linspace(0,L,161)
-    y1 = u(x,frame)
-    # y2 = net(torch.tensor([frame * 1.]))
-    y1 = y1.detach().numpy()
-    y2 = y2.detach().numpy()
-    line1.set_data(x,y1)
-    line2.set_data(x,y2)
+def init():
+    line1.set_data([],[])
+    line2.set_data([],[])
     return line1,line2
 
 
 
-ani = FuncAnimation(fig, update, frames = 800, blit=True, interval=1)
+def update(frame):
+
+
+    x = torch.linspace(0,L,81)
+    y1 = u(x,frame)
+    y1 = y1.detach().numpy()
+    line1.set_data(x,y1)
+    y2 = net(core_1d.train_input[frame])
+    y2 = y2.detach().numpy()
+    line2.set_data(x,y2)
+    return line1,line2
+
+
+                                #correct frames to core_1d.data_range
+ani = FuncAnimation(fig, update, frames = core_1d.data_range, blit=True, interval=1)
 plt.show()
