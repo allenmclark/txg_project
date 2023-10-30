7fead126eed349b9dbacccae86347caa54fa3047
Clark <631441@bah.com>
2023-10-30 15:21:45 -0500

    updated 3pm  10/30/2023

--- core_1d.py
+++ core_1d.py
@@ -6,22 +6,28 @@ K = .95
 L = 80
 c = (K/(sigma*rho))**.5
 
+
 def lam(n):
     return c * n * torch.pi / L
 
+# u() is the analytical solution to our 1d heat equation problem
 def u(x,t):
     return 100 * torch.e**(-lam(1)**2*t) * torch.sin((torch.pi/L)*x)
 
+print(u(torch.tensor([30,40]),torch.tensor([[2,3]])))
 
-
+# how many seconds the data is trained for
 data_range = 5
-num_heat_points = 161
 
-x = torch.linspace(0,80,num_heat_points) #change back to 161 later
+# how many points along the 1d bar are sampled for training
+num_heat_points = 81
 
+#tensor of all the points sampled for heat training 
+x = torch.linspace(0,80,num_heat_points)
+
+# initialiazed 1d tensors for building our input and output datasets 
 train_input = torch.tensor([])
 train_output = torch.tensor([])
-
 
 for t in range(data_range):
     for x_pos in x:
@@ -31,20 +37,19 @@ for t in range(data_range):
         train_output = torch.cat((train_output,torch.tensor([[u(x_pos,t)]])))
 
 
-print(train_output.shape)
-
-print('input',train_input[:4])
-print('output',train_output[:4])
+print('input',train_input.shape)
+print('output',train_output.shape)
 
 time = torch.tensor([])
-position = torch.tensor([])
+position = torch.tensor([[]])
 
-for val in train_input:
-    time = torch.cat((time,torch.tensor([val[1]])))
-    position = torch.cat((position,torch.tensor([val[0]])))
+# for val in train_input:
+#     position = torch.cat((position,torch.tensor([[val[0]]])))
+#     time = torch.cat((time,torch.tensor([val[1]])))
 
-print('time',time[:4])
-print('position',position[0:4])
+
+# print('time',time[:4])
+# print('position',position)
 
 
 
