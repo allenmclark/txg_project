27f936733cfa1e894c936e038df80408feb0c3a1
Clark <631441@bah.com>
2023-10-30 11:52:37 -0500

    time and position inputs separated into different arrays

--- core_1d.py
+++ core_1d.py
@@ -14,26 +14,37 @@ def u(x,t):
 
 
 
-train_til_time = 30
+data_range = 5
 num_heat_points = 161
 
 x = torch.linspace(0,80,num_heat_points) #change back to 161 later
 
-
-train_output = torch.tensor([])
 train_input = torch.tensor([])
-final_input = torch.tensor([])
-for t in range(train_til_time):
-    
-
-    train_input = torch.cat((torch.tensor([t]),x),dim=0)
-    train_input = train_input.view(1,num_heat_points + 1)
-    final_input = torch.cat((final_input,train_input),dim=0)
+train_output = torch.tensor([])
 
 
-    train_output = torch.cat((train_output,u(x,t).view(1,num_heat_points)))
+for t in range(data_range):
+    for x_pos in x:
+        train_input = torch.cat((train_input,torch.tensor([[x_pos,t]])))
+                                
+        #train_output = torch.cat((train_output,u(x,t).view(1,num_heat_points)))
+        train_output = torch.cat((train_output,torch.tensor([[u(x_pos,t)]])))
+
 
 print(train_output.shape)
+
+print('input',train_input[:4])
+print('output',train_output[:4])
+
+time = torch.tensor([])
+position = torch.tensor([])
+
+for val in train_input:
+    time = torch.cat((time,torch.tensor([val[1]])))
+    position = torch.cat((position,torch.tensor([val[0]])))
+
+print('time',time[:4])
+print('position',position[0:4])
 
 
 
